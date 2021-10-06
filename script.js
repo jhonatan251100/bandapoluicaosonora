c2f6d26c955b4b482824c73362e4b3e5c881ddcc
jhonatan251100 <jhonatan_chapolin@hotmail.com>
2021-10-05 23:06:13 -0300

    ATUALIZAÇÕES

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ var menu=document.querySelector(".menu");
 menu.addEventListener("click",function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+var nome = window.prompt('Qual é seu nome?')
+window.alert('BEM VINDO ao SITE da BANDA POLUIÇÃO SONORA ,'   + nome )
